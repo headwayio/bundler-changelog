0c302a1fe869cdbd6d0d074e03e5de54f3f67677
Noah Settersten <noah@headway.io>
2023-06-09 14:12:12 -0500

    chore: Disable unused spec

--- spec/bundler/changelog_spec.rb
+++ spec/bundler/changelog_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Bundler::Changelog do
     expect(Bundler::Changelog::VERSION).not_to be_nil
   end
 
-  it "does something useful" do
-    expect(false).to be(true)
-  end
+  # it "does something useful" do
+  #   expect(false).to be(true)
+  # end
 end
