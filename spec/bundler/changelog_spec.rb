df6fadd9c09c8164fa6bc4b6ef6e4e622e9c782a
Noah Settersten <noah@headway.io>
2023-06-10 11:47:24 -0500

    test: Require version in main spec

--- spec/bundler/changelog_spec.rb
+++ spec/bundler/changelog_spec.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require_relative "../../lib/bundler/changelog/version"
+
 RSpec.describe Bundler::Changelog do
   it "has a version number" do
     expect(Bundler::Changelog::VERSION).not_to be_nil
